0e70be5813436359253c7f4a53e8a9b189763756
Leo Miranda <contato@leomiranda.com>
2021-08-06 17:55:41 -0400

    aula 10

--- src/components/foundation/layout/Grid/index.js
+++ src/components/foundation/layout/Grid/index.js
@@ -1,5 +1,39 @@
 import styled, { css } from 'styled-components'
 import { breakpointsMedia } from '../../../../theme/utils/breakpointsMedia'
+import { propToStyle } from '../../../../theme/utils/propToStyle'
+
+const Container = styled.div`
+	width: 100%;
+	padding-right: 28px;
+	padding-left: 28px;
+	margin-right: auto;
+	margin-left: auto;
+
+	${breakpointsMedia({
+		xs: css`
+			max-width: initial;
+
+			padding-right: 28px;
+			padding-left: 28px;
+		`,
+		sm: css`
+			max-width: 576px;
+		`,
+		md: css`
+			max-width: 768px;
+			padding-right: 16px;
+			padding-left: 16px;
+		`,
+		lg: css`
+			max-width: 1160px;
+		`,
+		xl: css`
+			max-width: 1222px;
+		`,
+	})}
+
+	${propToStyle('marginTop')}
+`
 
 const Col = styled.div`
 	padding-right: 16px;
@@ -7,6 +41,9 @@ const Col = styled.div`
 	flex-basis: 0;
 	flex-grow: 1;
 	max-width: 100%;
+
+	${propToStyle('marginBottom')}
+
 	${({ value }) => {
 		if (typeof value === 'number') {
 			return css`
@@ -47,6 +84,7 @@ const Col = styled.div`
 			}),
 		})
 	}}
+
 	${({ offset }) => {
 		if (typeof offset === 'number') {
 			return css`
@@ -81,6 +119,11 @@ const Col = styled.div`
 			}),
 		})
 	}}
+
+  ${propToStyle('display')}
+  ${propToStyle('alignItems')}
+  ${propToStyle('justifyContent')}
+  ${propToStyle('flexDirection')}
 `
 
 Col.defaultProps = {
@@ -89,36 +132,7 @@ Col.defaultProps = {
 }
 
 export const Grid = {
-	Container: styled.div`
-		width: 100%;
-		padding-right: 28px;
-		padding-left: 28px;
-		margin-right: auto;
-		margin-left: auto;
-
-		${breakpointsMedia({
-			xs: css`
-				max-width: initial;
-
-				padding-right: 28px;
-				padding-left: 28px;
-			`,
-			sm: css`
-				max-width: 576px;
-			`,
-			md: css`
-				max-width: 768px;
-				padding-right: 16px;
-				padding-left: 16px;
-			`,
-			lg: css`
-				max-width: 1160px;
-			`,
-			xl: css`
-				max-width: 1222px;
-			`,
-		})}
-	`,
+	Container,
 	Row: styled.div`
 		display: flex;
 		flex-wrap: wrap;
