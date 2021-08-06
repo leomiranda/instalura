2dfe54ebc195ec0279ed9f7b8abac4501f603d33
Leo Miranda <contato@leomiranda.com>
2021-08-05 22:08:24 -0400

    aula 08

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components'
 import get from 'lodash/get'
 import { TextStyleVariantsMap } from '../../foundation/Text'
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia'
+import { propToStyle } from '../../../theme/utils/propToStyle'
 
 const ButtonGhost = css`
 	color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
@@ -42,4 +43,7 @@ export const Button = styled.button`
 			${TextStyleVariantsMap.paragraph1}
 		`,
 	})}
+
+	${propToStyle('display')}
+	${propToStyle('margin')}
 `
