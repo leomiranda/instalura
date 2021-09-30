280656177b7ab1bb0255af9cf22dd6632f94ebe1
Leo Miranda <contato@leomiranda.com>
2021-09-30 09:56:41 -0400

    test(cypress): refactor code

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,26 +1,50 @@
 /// <reference types="cypress" />
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
 
 describe('/pages/app/login', () => {
-  it('preencha os campos e vá para a página /app/profile', () => {
-    cy.intercept(
-      'https://instalura-api-git-master-omariosouto.vercel.app/api/login'
-    ).as('userLogin');
+  describe('when fill and submit a form login request', () => {
+    it('go to the profile page', () => {
+      // Pré Teste
+      cy.intercept(
+        'https://instalura-api-git-master.omariosouto.vercel.app'
+      ).as('userLogin');
 
-    cy.visit('/app/login');
-    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
-    cy.get('#formCadastro button[type="submit"]').click();
+      // Cenário
+      const loginScreen = new LoginScreenPageObject(cy);
+      loginScreen
+        .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+        .submitLoginForm();
 
-    cy.url().should('include', 'app/profile/');
+      // Asserções
+      cy.url().should('include', '/app/profile');
 
-    // Temos o token?
-    cy.wait('@userLogin').then((intercept) => {
-      // token do servidor
-      const { token } = intercept.response.body.data;
-      cy.getCookie('APP_TOKEN')
-        .should('exist')
-        // token do cookie é igual ao do server?
-        .should('have.property', 'value', token);
+      cy.wait('@userLogin').then((intercept) => {
+        const { token } = intercept.response.body.data;
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          .should('have.property', 'value', token);
+      });
     });
   });
+
+  // it('preencha os campos e vá para a página /app/profile', () => {
+  //   cy.intercept(
+  //     'https://instalura-api-git-master-omariosouto.vercel.app/api/login'
+  //   ).as('userLogin');
+
+  //   const loginScreen = new LoginScreenPageObject(cy);
+
+  //   loginScreen
+  //     .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+  //     .submitLoginForm();
+
+  //   cy.url().should('include', 'app/profile/');
+
+  //   cy.wait('@userLogin').then((intercept) => {
+  //     const { token } = intercept.response.body.data;
+  //     cy.getCookie('APP_TOKEN')
+  //       .should('exist')
+  //       .should('have.property', 'value', token);
+  //   });
+  // });
 });
